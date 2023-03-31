36d40daf5b0b2c413a63fd3889837a73a3221037
romardoss <romassuper@gmail.com>
2023-03-31 15:52:45 +0300

    cipher manager update

--- SystemSecurityLabWorks/Cipher/CipherManager.cs
+++ SystemSecurityLabWorks/Cipher/CipherManager.cs
@@ -5,85 +5,61 @@ namespace SystemSecurityLabWorks.Cipher
 {
     public class CipherManager
     {
-        public static string Encrypt(string input, string key)
+        public static string[] ciphers = {"Caesar", 
+            "Tritemius (linear)", 
+            "Tritemius (squaire)",
+            "Tritemius (slogan)", 
+        };
+
+        public static string Encrypt(string input, string key, string cipher)
         {
-            string[] keys = key.Split();
-            bool isAllNumbers = true;
-            for (int i = 0; i < keys.Length; i++)
+            switch (cipher)
             {
-                if (!int.TryParse(keys[i], out _))
-                {
-                    isAllNumbers = false;
-                }
-            }
-            if (!isAllNumbers)
-            {
-                var gaslo = new SloganCipher();
-                return gaslo.Encrypt(input, key);
-            }
-            switch (keys.Length)
-            {
-                case 1:
-                    {
-                        var caesar = new CaesarCipher();
-                        return caesar.Encrypt(input, key);
-                    }
-                case 2:
-                    {
-                        var linear = new LinearCipher();
-                        return linear.Encrypt(input, key);
-                    }
-                case 3:
-                    {
-                        var nonelinear = new NonLinearCipher();
-                        return nonelinear.Encrypt(input, key);
-                    }
-                default:
-                    {
-                        MessageBox.Show("Invalid key");
-                        return input;
-                    }
+                case "Caesar":
+                    var caesar = new CaesarCipher();
+                    return caesar.Encrypt(input, key);
+
+                case "Tritemius (linear)":
+                    var tritemius1 = new LinearCipher();
+                    return tritemius1.Encrypt(input, key);
+
+                case "Tritemius (squaire)":
+                    var tritemius2 = new NonLinearCipher();
+                    return tritemius2.Encrypt(input, key);
+
+                case "Tritemius (slogan)":
+                    var tritemius3 = new SloganCipher();
+                    return tritemius3.Encrypt(input, key);
+
+                default: 
+                    MessageBox.Show("Wrong cipher chosen");
+                    return "Wrong cipher chosen";
             }
         }
 
-        public static string Decrypt(string input, string key)
+        public static string Decrypt(string input, string key, string cipher)
         {
-            string[] keys = key.Split();
-            bool isAllNumbers = true;
-            for (int i = 0; i < keys.Length; i++)
+            switch (cipher)
             {
-                if (!int.TryParse(keys[i], out _))
-                {
-                    isAllNumbers = false;
-                }
-            }
-            if (!isAllNumbers)
-            {
-                var gaslo = new SloganCipher();
-                return gaslo.Decrypt(input, key);
-            }
-            switch (keys.Length)
-            {
-                case 1:
-                    {
-                        var caesar = new CaesarCipher();
-                        return caesar.Decrypt(input, key);
-                    }
-                case 2:
-                    {
-                        var linear = new LinearCipher();
-                        return linear.Decrypt(input, key);
-                    }
-                case 3:
-                    {
-                        var nonelinear = new NonLinearCipher();
-                        return nonelinear.Decrypt(input, key);
-                    }
+                case "Caesar":
+                    var caesar = new CaesarCipher();
+                    return caesar.Decrypt(input, key);
+
+                case "Tritemius (linear)":
+                    var tritemius1 = new LinearCipher();
+                    return tritemius1.Decrypt(input, key);
+
+                case "Tritemius (squaire)":
+                    var tritemius2 = new NonLinearCipher();
+                    return tritemius2.Decrypt(input, key);
+
+                case "Tritemius (slogan)":
+                    var tritemius3 = new SloganCipher();
+                    return tritemius3.Decrypt(input, key);
+
                 default:
-                    {
-                        MessageBox.Show("Invalid key");
-                        return input;
-                    }
+                    MessageBox.Show("Wrong cipher chosen");
+                    return "Wrong cipher chosen";
             }
         }
     }
--- SystemSecurityLabWorks/MainWindow.xaml.cs
+++ SystemSecurityLabWorks/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using SystemSecurityLabWorks.Cipher;
 using System.Windows;
 using SystemSecurityLabWorks.UIControllers;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
 
 namespace SystemSecurityLabWorks
 {
@@ -26,7 +28,7 @@ namespace SystemSecurityLabWorks
         private void EncryptButton_Click(object sender, RoutedEventArgs e)
         {
             string input = WorkingText.Text;
-            var answer = CipherManager.Encrypt(input, KeyText.Text);
+            var answer = CipherManager.Encrypt(input, KeyText.Text, CipherChecked());
             var fileController = new FileController();
             fileController.PrintFile(WorkingText, answer);
         }
@@ -34,9 +36,25 @@ namespace SystemSecurityLabWorks
         private void DecryptButton_Click(object sender, RoutedEventArgs e)
         {
             string input = WorkingText.Text;
-            var answer = CipherManager.Decrypt(input, KeyText.Text);
+            var answer = CipherManager.Decrypt(input, KeyText.Text, CipherChecked());
             var fileController = new FileController();
             fileController.PrintFile(WorkingText, answer);
+        }
+
+        private string CipherChecked()
+        {
+            RadioButton[] radioButtons = { CaesarRadioButton, 
+                TritemiusLinearRadioButton, TritemiusSquaireRadioButton,
+                TritemiusSloganRadioButton};
+
+            foreach (var item in radioButtons)
+            {
+                if((bool)item.IsChecked) 
+                {
+                    return item.Content.ToString();
+                }
+            }
+            return null;
         }
 
         private void AboutButton_Click(object sender, RoutedEventArgs e)
